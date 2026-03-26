678cef99ed51b11b8dbb10bfc5df05aca47c4858
rachithak <rachitha102007@gmail.com>
2026-03-26 21:53:48 +0530

    added right_triangle

---
+++ right_triangle.py
@@ -0,0 +1,7 @@
+num=int(input("enter number:"))
+for i in range(1,num+1):
+    for j in range(1,num-i+1):
+        print(" ",end=" ")
+    for k in range(1,i+1):
+        print("*",end=" ")
+    print()
