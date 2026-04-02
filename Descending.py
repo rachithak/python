fa3caa494d27a1e734dc4a4bd72e562bfa582cdb
rachithak <rachitha102007@gmail.com>
2026-04-02 21:17:05 +0530

    added descending order

---
+++ Descending.py
@@ -0,0 +1,14 @@
+marks=[]
+for i in range (6):
+    m=int(input("enter marks:"))
+    marks.append(m)
+    n=len(marks)
+for i in range(n):
+    for j in range(0,n-i-1):
+        if marks[j] < marks[j+1]:
+            temp=marks[j]
+            marks[j]=marks[j+1]
+            marks[j+1]=temp
+print("marks from highest to lowest")
+for m in marks:
+    print(m)
