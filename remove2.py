370e3e2be4ca2d875d35be15801873274af82661
RACHITHA K <rachitha102007@gmail.com>
2026-04-11 23:27:42 +0530

    added punctuation remove without split

---
+++ remove2.py
@@ -0,0 +1,14 @@
+import string
+def remove_punctuation(my_story):
+    phrase_sans_punct=""
+    for letter in my_story:
+        if letter not in string.punctuation:
+            phrase_sans_punct+=letter
+    return phrase_sans_punct
+my_story="""
+the way of the program: the python programming language,what is a program?what is debugging?
+syntax errors,runtime error,sematic errors,experimental debugging.
+variables,etc.
+"""
+words=remove_punctuation(my_story)
+print(words)
