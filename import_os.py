cf06ce289ab9169d4487f8cbe3553868cb7a6de1
RACHITHA K <rachitha102007@gmail.com>
2026-05-05 22:08:32 +0530

    added import os

---
+++ import_os.py
@@ -0,0 +1,18 @@
+import os
+def get_dirlist(path):
+    dirlist=os.listdir(path)
+    dirlist.sort()
+    return dirlist
+def print_files(path,prefix=""):
+    if prefix=="":
+        print("folder listing for",path)
+        prefix="| "
+    dirlist=get_dirlist(path)
+    for file in dirlist:
+        print(prefix+file)
+        fullname=os.path.join(path,file)
+        if os.path.isdir(fullname):
+            print_files(fullname,prefix+"|")
+path="c:\\user\\rachitha\\Desktop"
+print(path)
+print_files(path)
