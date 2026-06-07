e42784fe519ec9f8d347810648b7567d71915580
RACHITHA K <rachitha102007@gmail.com>
2026-06-07 22:10:30 +0530

    added class student

---
+++ class.py
@@ -0,0 +1,10 @@
+class student:
+    college="abc college"
+    def __init__(self,name):
+        self.name=name
+s1=student("ravi")
+s2=student("priya")
+print(s1.college)
+print(s2.college)
+print(s1.name)
+print(s2.name)
