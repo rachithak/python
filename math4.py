6af19400a7fe1fc0b838dbf77cda5af5dabe6178
RACHITHA K <rachitha102007@gmail.com>
2026-06-12 22:50:21 +0530

    added

--- math4.py
+++ math4.py
@@ -1,3 +1,17 @@
-import math
-math=math.radians(90)
-print(math)
+def DIV(a,b):
+    assert a>0,f"asserton error:'a' must be greater than 0,but got a={a}"
+    if b==0:
+        raise valueerror("valuerror:'b' cannot be zero_division by zero is undefined")
+        c=a/b
+        return c
+try:
+    a=float(input("enter value of a:"))
+    b=float(input("enter value of b:"))
+    result=DIV(a,b)
+    print(f"result c={a}/{b}={result}")
+except assertionerror as ae:
+    print(f"assertion failed->{ae}")
+except valueerror as ve:
+    print(f"value error caught->{ve}")
+except exception as e:
+    print(f"unexpected error->{e}")
