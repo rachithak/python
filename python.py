17c63781c602153548bff2c867ef4c29beaf3d09
RACHITHA K <rachitha102007@gmail.com>
2026-08-17 22:07:25 +0530

    added

--- python.py
+++ python.py
@@ -1,5 +1,5 @@
-for i in range(1,101):
-    if i%3==0 and i%5==0:
-        continue
-    if(i%3==0 or i%5==0):
-        print(i,end=" ")
+n=5
+while n>=0:
+    for i in range n:
+        n=n+1
+print("number:",n)
