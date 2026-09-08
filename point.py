c22f28e0915e8d90bfb22b9001d9cbf94a1c0173
RACHITHA K <rachitha102007@gmail.com>
2026-09-08 22:18:38 +0530

    added

--- point.py
+++ point.py
@@ -1,3 +1,16 @@
-import math
-math=math.radians(-180)
-print(math)
+def factorial_module(n):
+    fact = 1
+    for i in range(1, n+1):
+        fact *= i
+    return fact
+import factorial_module
+
+n = 5
+r = 2
+
+result = factorial_module.factorial(n) // (
+    factorial_module.factorial(r) *
+    factorial_module.factorial(n-r)
+)
+
+print("Binomial Coefficient =", result)
