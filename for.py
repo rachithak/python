e1a52f38a7236dd868ba2a2750ccf0233fb804e0
RACHITHA K <rachitha102007@gmail.com>
2026-09-24 21:14:57 +0530

    added

--- for.py
+++ for.py
@@ -1,3 +1,7 @@
-word="banana"
-for letter in word:
-    print(letter)
+def remove_vowels(phrase):
+    vowels="aeiou"
+    string_sans_vowels=""
+    for letter in phrase:
+        if letter.lower()not in vowels:
+        string_sans_vowels+=letter
+    return string_sans_vowels
